1f7c53a28ca6e6093902f0464e65cd85152c908a
bnbnac <bnbnac@naver.com>
2023-08-13 14:45:25 +0900

    example for portected middleware. but we will use the morgan

--- src/server.js
+++ src/server.js
@@ -3,25 +3,31 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-app.listen(PORT, () =>
-  console.log(`server is listening on http://localhost:${PORT}`)
-);
-
-const globalMiddleware = (req, res, next) => {
-  console.log("hello from global");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
   next();
 };
 
-const secondMiddleware = (req, res, next) => {
-  console.log("hello from second");
+const privateMiddleware = (req, res, next) => {
+  const path = req.path;
+  if (path === "/protected") {
+    return res.send("<h1>not allowed</h1>");
+  }
   next();
 };
 
-const finalController = (req, res) => {
+const handleHome = (req, res) => {
   return res.send("hello " + req.headers["user-agent"]);
 };
 
-app.get("/former", finalController);
-app.use(globalMiddleware); // global middleware. note the position
-app.get("/", globalMiddleware, secondMiddleware, finalController);
-app.get("/later", finalController);
+const handleProtected = (req, res) => {
+  return res.send("private area");
+};
+
+app.listen(PORT, () =>
+  console.log(`server is listening on http://localhost:${PORT}`)
+);
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
