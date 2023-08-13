d4b3d6bac7132145f4ef35770cf58ad151d07757
bnbnac <bnbnac@naver.com>
2023-08-13 18:54:00 +0900

    when use params, be careful of the order of routes

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("homepage videos");
-export const watch = (req, res) => res.send("watch video");
-export const edit = (req, res) => res.send("edit video");
+export const see = (req, res) => res.send("see video " + req.params.id);
+export const edit = (req, res) => res.send("edit video " + req.params.id);
+export const search = (req, res) => res.send("search video");
+export const remove = (req, res) => res.send("remove video " + req.params.id);
+export const upload = (req, res) => res.send("upload video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { edit, watch } from "../controllers/videoController";
+import { edit, remove, see, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload); // routig code using params should be bottom
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/remove", remove);
 
 export default videoRouter;
