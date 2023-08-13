9ca5a6c7b63a391dbd996d532b26cc5608a594c4
bnbnac <bnbnac@naver.com>
2023-08-13 20:41:44 +0900

    introduce pug. see include, extends, block

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.send("homepage videos");
-export const see = (req, res) => res.send("see video " + req.params.id);
-export const edit = (req, res) => res.send("edit video " + req.params.id);
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search video");
 export const remove = (req, res) => res.send("remove video " + req.params.id);
 export const upload = (req, res) => res.send("upload video");
