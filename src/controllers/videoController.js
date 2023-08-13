2dbc58ed8cabb28cdda2c1d9781f903bd4092f18
bnbnac <bnbnac@naver.com>
2023-08-13 20:53:49 +0900

    pass variables from controller to template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("search video");
 export const remove = (req, res) => res.send("remove video " + req.params.id);
 export const upload = (req, res) => res.send("upload video");
