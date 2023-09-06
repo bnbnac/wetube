49685dc1f478c9987c3c2e821c1f7765142653be
bnbnac <bnbnac@naver.com>
2023-09-07 08:53:26 +0900

    post data from frontend

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,37 +3,55 @@ const fakeUser = {
   loggedIn: true,
 };
 
+let videosOnFakeDB = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  res.render("home", { pageTitle: "Home", fakeUser, videos });
+  res.render("home", { pageTitle: "Home", fakeUser, videosOnFakeDB });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videosOnFakeDB[id - 1];
+  res.render("watch", { video, pageTitle: `Watching: ${video.title}` });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videosOnFakeDB[id - 1];
+  res.render("edit", { video, pageTitle: `Editing: ${video.title}` });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  // change the title in the (real)database here
+  videosOnFakeDB[id - 1].title = title;
+
+  res.redirect(`/videos/${id}`);
+};
 export const search = (req, res) => res.send("search video");
 export const remove = (req, res) => res.send("remove video " + req.params.id);
 export const upload = (req, res) => res.send("upload video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { edit, remove, see, upload } from "../controllers/videoController";
+import {
+  getEdit,
+  postEdit,
+  remove,
+  upload,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/remove", remove);
 videoRouter.get("/upload", upload);
 
